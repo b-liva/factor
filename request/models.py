9a86619cfde34ee4fc30ac9f0e961122415761a2
b-liva <ebammo20@gmail.com>
2018-09-18 00:34:22 -0700

    request app with prefactor, prefactor verification added

---
+++ request/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Requests(models.Model):
+    image = models.FileField('requests/')
+    summary = models.TextField(max_length=1000)
+
+
+class Prefactor(models.Model):
+    request_id = models.ForeignKey(Requests, on_delete=models.CASCADE)
+    image = models.ImageField('requests/prefactors/original/')
+    summary = models.TextField(max_length=1000)
+
+
+class PrefactorVerification(models.Model):
+    pref_id = models.ForeignKey(Prefactor, on_delete=models.CASCADE)
+    image = models.ImageField('requests/prefactors/verifications/')
+    summary = models.TextField(max_length=1000)
+
