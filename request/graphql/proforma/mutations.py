773d54f7302dcd7742e969a05beb250120cc575f
name <ebammo20@gmail.com>
2020-02-18 12:34:08 +0330

    Editing proforma gq mutation names.

--- request/graphql/proforma/mutations.py
+++ request/graphql/proforma/mutations.py
@@ -3,16 +3,16 @@ from graphene_django.forms.mutation import DjangoModelFormMutation
 from ..forms.forms import ProformaModelForm, PrefSpecForm
 
 
-class ProformaModelMutation(DjangoModelFormMutation):
+class ProformaModelFormMutation(DjangoModelFormMutation):
     class Meta:
         form_class = ProformaModelForm
 
 
-class PrefSpecModelMutation(DjangoModelFormMutation):
+class PrefSpecModelFormMutation(DjangoModelFormMutation):
     class Meta:
         form_class = PrefSpecForm
 
 
 class ProformaModelMutation(ObjectType):
-    proforma_mutation = ProformaModelMutation.Field()
-    prefspec_mutation = PrefSpecModelMutation.Field()
+    proforma_mutation = ProformaModelFormMutation.Field()
+    prefspec_mutation = PrefSpecModelFormMutation.Field()
