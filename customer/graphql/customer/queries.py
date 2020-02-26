152a56b69c30012438265d9e93b80dd54775e2d5
name <ebammo20@gmail.com>
2020-02-26 12:57:19 +0330

    adding custom filter set with pk field to customer. both pk and gID work

--- customer/graphql/customer/queries.py
+++ customer/graphql/customer/queries.py
@@ -2,12 +2,13 @@ import graphene
 from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
 
-from .types import CustomerNode, CustomerTypeNode
+from .types import CustomerNode, CustomerTypeNode, CustomerFilterSet
 
 
-class Query(object):
+# class Query(object):
+class Query(graphene.ObjectType):
     customer = relay.Node.Field(CustomerNode)
-    all_customers = DjangoFilterConnectionField(CustomerNode)
+    all_customers = DjangoFilterConnectionField(CustomerNode, filterset_class=CustomerFilterSet)
 
     customer_type = relay.Node.Field(CustomerTypeNode)
     all_customer_types = DjangoFilterConnectionField(CustomerTypeNode)
--- customer/graphql/customer/types.py
+++ customer/graphql/customer/types.py
@@ -1,15 +1,31 @@
+import django_filters
 import graphene
-from graphene import relay
+from graphene import relay, Node
 from graphene_django.types import DjangoObjectType
+from graphql_relay import from_global_id
 
 from customer.models import Customer, Type
 
 
-class CustomerNode(DjangoObjectType):
+class CustomerFilterSet(django_filters.FilterSet):
+    pk = django_filters.NumberFilter(field_name='pk')
 
     class Meta:
         model = Customer
+        fields = {
+            'id': ['exact'],
+            'name': ['exact', 'icontains']
+        }
+
+
+class CustomerNode(DjangoObjectType):
+    pk = graphene.Field(type=graphene.Int, source='id')
+
+    class Meta:
+        model = Customer
+        # filter_fields is now a duplication and will be deleted.
         filter_fields = {
+            'id': ['exact'],
             'name': ['exact', 'icontains']
         }
         interfaces = (relay.Node,)
