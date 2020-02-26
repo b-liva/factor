673df3cc3a54b327290224876cdc7ffbc7ee0ce1
name <ebammo20@gmail.com>
2020-02-26 13:00:06 +0330

    defining proformafilterset

--- request/graphql/proforma/queries.py
+++ request/graphql/proforma/queries.py
@@ -1,11 +1,11 @@
 from graphene import relay
 from graphene_django.filter import DjangoFilterConnectionField
-from .types import ProformaNode, PrefSpecNode
+from .types import ProformaNode, PrefSpecNode, ProformaFilterSet
 
 
 class Query(object):
     proforma = relay.Node.Field(ProformaNode)
-    all_proformas = DjangoFilterConnectionField(ProformaNode)
+    all_proformas = DjangoFilterConnectionField(ProformaNode, filterset_class=ProformaFilterSet)
 
     pref_spec = relay.Node.Field(PrefSpecNode)
     all_pref_specs = DjangoFilterConnectionField(PrefSpecNode)
--- request/graphql/proforma/types.py
+++ request/graphql/proforma/types.py
@@ -1,7 +1,21 @@
+import django_filters
 import graphene
 from graphene import relay
 from graphene_django import DjangoObjectType
 from request.models import Xpref, PrefSpec
+
+
+class ProformaFilterSet(django_filters.FilterSet):
+    # This will be added to fields by default as ['exact'] so there is no need to add it explicitly.
+    customer_pk = django_filters.NumberFilter(field_name='req_id__customer__id')
+
+    class Meta:
+        model = Xpref
+        fields = {
+            'number': ['exact'],
+            'number_td': ['exact'],
+            'req_id__customer__name': ['icontains']
+        }
 
 
 class ProformaNode(DjangoObjectType):
