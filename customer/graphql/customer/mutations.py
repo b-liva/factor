d17a6dcffc7d46a537e304a95815ffea546f0dfe
name <ebammo20@gmail.com>
2020-05-17 12:02:25 +0430

    Adding customer gql mutation

--- customer/graphql/customer/mutations.py
+++ customer/graphql/customer/mutations.py
@@ -1,15 +1,18 @@
 import graphene
-from graphene import relay, ObjectType
+from django.core.exceptions import ValidationError
+from graphene import relay, ObjectType, InputObjectType
 from graphene_django import DjangoObjectType
 from graphene_django.forms.mutation import DjangoModelFormMutation, DjangoFormMutation
-
+from graphene_django.rest_framework.mutation import SerializerMutation
 from graphql_relay import from_global_id
 
 from .types import CustomerNode
 from customer.forms import CustomerForm
+from .types import CustomerNode
+from customer.drf.serializers import CustomerSerializer
 
 from customer.models import Customer
-from django import forms
+from django import forms, http
 
 
 class MyForm(forms.Form):
@@ -21,21 +24,67 @@ class MyMutation(DjangoFormMutation):
     class Meta:
         form_class = MyForm
 
+    # probably this is the choice with django forms
+    # @classmethod
+    # def perform_mutate(cls, form, info):
+    #     pass
+
+    # And this is the choice with relay.
     @classmethod
     def mutate_and_get_payload(cls, root, info, **input):
         return cls(test_field=input.get('test_field'))
 
 
-class MyModelForm(forms.ModelForm):
+class CustomerModelform(forms.ModelForm):
+    # customer_id = forms.CharField()
+
+    def __init__(self, data=None, *args, **kwargs):
+        # data = kwargs.get('data')
+        print('this is data: ', data)
+        # instance = kwargs.get('instance', None)
+        # if instance:
+        #     print(instance.pk)
+        attrs = ['owner', 'type']
+        if data is not None:
+            for attr in attrs:
+                if attr in data:
+                    print('attr: ', attr)
+                    data[attr] = from_global_id(data[attr])[1]
+            # if 'customer_id' in data:
+            #     # data['pk'] = from_global_id(data['customer_id'])[1]
+            #     print(data)
+        print('new data', data)
+        super(CustomerModelform, self).__init__(data, args, kwargs)
+
     class Meta:
         model = Customer
         fields = "__all__"
 
 
-class MyModelFormMutation(DjangoModelFormMutation):
+    # def clean(self):
+    #     print('clean', self.cleaned_data)
+    #
+    #     print(self.changed_data)
+    #     return super(MyModelForm, self).clean()
+
+    # def clean_date2(self):
+    #     print('cleaning owner')
+
+    # def is_valid(self):
+    #     print('form validation')
+    #     return super().is_valid()
+
+
+class CustomerType(DjangoObjectType):
+    class Meta:
+        model = Customer
+
+
+class CreateCustomer(DjangoModelFormMutation):
+    customer = relay.node.Field(CustomerNode)
 
     class Meta:
-        form_class = MyModelForm
+        form_class = CustomerModelform
 
     # @classmethod
     # def mutate_and_get_payload(cls, root, info, **input):
@@ -44,12 +93,116 @@ class MyModelFormMutation(DjangoModelFormMutation):
     #     form = cls.get_form(root, info, **input)
     #     print(form)
     #     return super().mutate_and_get_payload(root, info, **input)
+
     @classmethod
-    def perform_mutate(cls, form, info):
+    def perform_mutate(cls, form, info, **kwargs):
         print("This only runs when the form is valid")
         return super().perform_mutate(form, info)
 
 
+class CustomerCreateInput(InputObjectType):
+    name = graphene.String(required=False)
+    owner = graphene.String()
+    type = graphene.String()
+    date2 = graphene.String()
+    pub_date = graphene.DateTime()
+
+
+class UpdateCustomer(relay.ClientIDMutation):
+
+    class Input:
+        customer = graphene.Argument(CustomerCreateInput)
+        id = graphene.String(required=True)
+
+    def __init__(self, *args, **kwargs):
+        print('the update class', args, kwargs)
+
+        super(UpdateCustomer, self).__init__(args, kwargs)
+
+    # class Meta:
+    #     form_class = CustomerModelform
+
+    errors = graphene.List(graphene.String)
+    updated_customer = graphene.Field(CustomerNode)
+
+    # def perform_mutate(cls, form, info):
+    #     print(cls)
+    #     print(form)
+    #     super(UpdateCustomer, cls).perform_mutate(form, info)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        print(root)
+        print(info)
+        print('input: ', input)
+        try:
+            customer_instance = get_object(Customer, input['id'])
+            print('customer instance: ', customer_instance, customer_instance.pk)
+            input['customer']['id'] = input['id']
+            if customer_instance:
+                form = CustomerModelform(input['customer'] or None, instance=customer_instance)
+                print('form is valid: ', form.is_valid(), form.errors)
+                updated_customer = form.save(commit=False)
+                updated_customer.save()
+                customer_data = input.get('customer')
+                # updated_customer = update_create_instance(customer_instance, customer_data)
+                return cls(updated_customer=updated_customer)
+        except ValidationError as e:
+            # return an error if something wrong happens
+            return cls(updated_book=None, errors=get_errors(e))
+
+
+def get_object(object_name, relayId, otherwise=None):
+    try:
+        return object_name.objects.get(pk=from_global_id(relayId)[1])
+    except:
+        return otherwise
+
+
+def update_create_instance(instance, args, exception=['id']):
+    if instance:
+        [setattr(instance, key, value) for key, value in args.items() if key not in exception]
+
+    # caution if you literally cloned this project, then be sure to have
+    # elasticsearch running as every saved instance must go through
+    # elasticsearch according to the way this project is configured.
+    instance.save()
+
+    return instance
+
+
+def get_errors(e):
+    # transform django errors to redux errors
+    # django: {"key1": [value1], {"key2": [value2]}}
+    # redux: ["key1", "value1", "key2", "value2"]
+    fields = e.message_dict.keys()
+    messages = ['; '.join(m) for m in e.message_dict.values()]
+    errors = [i for pair in zip(fields, messages) for i in pair]
+    return errors
+
+
+class MyCustomerMutation(SerializerMutation):
+    class Meta:
+        serializer_class = CustomerSerializer
+        lookup_field = 'id'
+
+    updated_customer = graphene.Field(CustomerNode)
+
+    @classmethod
+    def get_serializer_kwargs(cls, root, info, **input):
+        if 'id' in input:
+            instance = Customer.objects.filter(
+                id=input['id']
+            ).first()
+            if instance:
+                return {'instance': instance, 'data': input, 'partial': True}
+            else:
+                raise http.Http404
+        return {'data': input, 'partial': True}
+
+
 class CustomerModelMutations(ObjectType):
     customer_mutation = MyMutation.Field()
-    customer_model_mutation = MyModelFormMutation.Field()
+    create_customer = CreateCustomer.Field()
+    update_customer_mut = UpdateCustomer.Field()
+    update_customer_ser = MyCustomerMutation.Field()
